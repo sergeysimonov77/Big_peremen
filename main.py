dc18db0835d99b3077b3af3d3a871feee1a81050
Simon <simonov-sergey@yandex.ry>
2023-06-12 22:19:32 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,48 @@
+# pip install sklearn
+# pip install pandas
+# pip install flask
+
+
+
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+'''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Симонов Виталий Большая перемена</title>
+    <style>
+        body {
+         background:rgba(9, 228, 228, 0.541);
+        }
+       </style>
+</head>
+<body>
+    <center><h1>Проект Коллективизация 2.0</h1>
+    <p>Данный проект создан в рамках конкурса "Большая перемена"
+        <br> Симонов Виталий МАОУ "СОШ №13 с УИОП" 8М город Электросталь Московской области
+    </p>
+    <form action="" method="post">
+    <h2>Введите название коллектива</h2>
+    <input type="text" name='jj'>
+    <button>Отправить</button></center>
+    </form>
+</body>
+</html>
+    '''
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
